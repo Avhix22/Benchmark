dfd95b1dc6febfdabd4117533a401e15d7c799c0
Avion Hicks <avvhix@Avions-MacBook-Air.local>
2026-04-15 13:26:04 -0400

    Add descriptive output for 64-bit floating point benchmark

--- float_benchmark.c
+++ float_benchmark.c
@@ -4,3 +4,7 @@
 int main() {
     clock_t start, end;
     double i, sum = 0.0, product = 1.0, quotient = 1.0;
+
+    printf("Benchmark 2:\n");
+    printf("64-bit Floating point operation benchmark\n");
+    printf("Which includes additions, multiplication, and division.\n");
