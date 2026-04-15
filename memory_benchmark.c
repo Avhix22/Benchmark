f9aac67d43db0f0e4fbfe8997166d41c1d0a8c50
Avion Hicks <avvhix@Avions-MacBook-Air.local>
2026-04-15 15:25:33 -0400

    Initialize large integer array for memory benchmark using dynamic allocation

--- memory_benchmark.c
+++ memory_benchmark.c
@@ -5,3 +5,9 @@ memory_benchmark.c
 above are my code. i created a file in my friends laptop what should i do now 
 #include <stdlib.h>
 #include <time.h>
+
+#define SIZE 5000000000LL
+
+int main() {
+    clock_t start, end;
+    int *arr = (int*) malloc(SIZE * sizeof(int));
