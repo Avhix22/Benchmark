ffd37d3c49a7cbf1deed3601ac6b220e282c5cd0
Avion Hicks <avvhix@Avions-MacBook-Air.local>
2026-04-15 13:44:42 -0400

    Add FILE_SIZE and CHUNK_SIZE definitions for benchmark configuration

--- float_benchmark.c
+++ float_benchmark.c
@@ -48,3 +48,6 @@ hard_drive_benchmark_2.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define FILE_SIZE 1000000000LL  // 1 GB
+#define CHUNK_SIZE 10000        // 10,000 bytes per read/write
