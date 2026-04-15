810842a4cea162a2c1eb0d2e3bffb5c247886e4b
Avion Hicks <avvhix@Avions-MacBook-Air.local>
2026-04-15 15:16:52 -0400

    Finalize benchmark timing calculation using CLOCKS_PER_SEC and print execution duration

--- hard_drive_benchmark_2.c
+++ hard_drive_benchmark_2.c
@@ -43,3 +43,9 @@ int main() {
     fclose(file);
 
     end = clock();
+
+    double execution_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Execution Time: %.6f seconds\n", execution_time);
+
+    return 0;
+}
