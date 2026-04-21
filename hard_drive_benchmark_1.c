90add84417001d349b1bb6c419bd1eb02cd126a0
Avion Hicks <avvhix@Avions-MacBook-Air.local>
2026-04-21 15:42:32 -0400

    Add main setup with file handling, buffer, and timing variables

--- hard_drive_benchmark_1.c
+++ hard_drive_benchmark_1.c
@@ -4,3 +4,8 @@
 
 #define FILE_SIZE 1000000000LL // 1 GB
 #define CHUNK_SIZE 100          // 100 bytes per read/write
+
+int main() {
+    clock_t start, end;
+    FILE *file;
+    char buffer[CHUNK_SIZE];
