3914f7a1376c8390f6115830fad0c6e7d7c8bd3f
Avion Hicks <avvhix@Avions-MacBook-Air.local>
2026-04-21 16:29:15 -0400

    Add benchmark screenshot results

--- memory_benchmark.c
+++ memory_benchmark.c
@@ -1,13 +1,9 @@
-memory_benchmark.c
-
 #include <stdio.h>
 
-above are my code. i created a file in my friends laptop what should i do now 
 #include <stdlib.h>
 #include <time.h>
 
-#define SIZE 5000000000LL
-
+#define SIZE 1000000000LL // 1 billion integers
 int main() {
     clock_t start, end;
     int *arr = (int*) malloc(SIZE * sizeof(int));
