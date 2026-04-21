1cde6fb92c391958a3ba0adfd9d8f401aeb7d3e0
Avion Hicks <avvhix@Avions-MacBook-Air.local>
2026-04-21 15:53:05 -0400

    Complete I/O benchmark by computing and printing execution time

--- hard_drive_benchmark_1.c
+++ hard_drive_benchmark_1.c
@@ -43,4 +43,10 @@ int main() {
 
     end = clock();
 
+    double execution_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Execution Time: %.6f seconds\n", execution_time);
+
+    return 0;
+}
+
         
